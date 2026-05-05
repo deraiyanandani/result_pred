63010d1a8e4323355e201a06ee0a4df6dd99112f
deraiyanandani <nrderaiya06@gmail.com>
2026-05-05 21:45:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from joblib import load
+import numpy as np
+
+# pip install fastapi uvicorn
+# python -m pip install numpy
+# python -m pip install joblib
+# python -m uvicorn app:app --reload
+
+app=FastAPI()
+
+model = load("multiclass2_api.joblib")
+
+# testing purpose
+@app.get("/")
+def testing():
+    return {"myapi:":"myapi server is working"}
+
+# data predict
+
+@app.get("/predict")
+def predict(study_hours: float):
+
+    input_data=np.array([[study_hours]])
+    predict = model.predict(input_data)
+
+    return{"study_hours":study_hours,"grade":float(predict[0])}
